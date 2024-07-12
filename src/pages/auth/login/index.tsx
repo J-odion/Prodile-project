a2e4ee066e6873d0c768b4a11b13c00f3b374db0
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2024-07-12 12:38:57 +0100

    fix: added signup

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -42,23 +42,23 @@ const Login = () => {
 
   return (
     <div className="relative flex items-center justify-center min-h-screen">
-      {/* <div className="absolute top-0 left-8 h-full w-1/4">
+      <div className="absolute md:block hidden md:w-1/2 top-0 left-8 h-full lg:w-1/4">
         <Image
           src="/images/agricFarm.svg"
           alt="Agricultural Illustration Left"
           layout="fill"
           objectFit="contain"
         />
-      </div> */}
-      <div className="absolute top-0 left-8 h-full w-1/2">
+      </div>
+      <div className="absolute hidden lg:block top-0 right-8 h-full w-1/4">
         <Image
-          src="/images/Saly-drive.svg"
+          src="/images/globe.svg"
           alt="Agricultural Illustration Right"
           layout="fill"
           objectFit="contain"
         />
       </div>
-      <Card className="mx-auto max-w-sm z-10 bg-transparent">
+      <Card className="mx-auto max-w-sm z-10 bg-transparent lg:bg-white">
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
           <CardDescription>
