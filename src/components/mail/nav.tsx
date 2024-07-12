8113e6775ef64d61dc2bb70cd8e9ca6fd915f947
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2024-07-12 11:32:07 +0100

    fix: added mail component

--- src/components/mail/nav.tsx
+++ src/components/mail/nav.tsx
@@ -22,44 +22,17 @@ interface NavProps {
 export function Nav({ links }: NavProps) {
   return (
     <div
-      // data-collapsed={isCollapsed}
       className="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2"
     >
       <nav className="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
         {links.map((link, index) =>
-          // isCollapsed ? (
-          //   <Tooltip key={index} delayDuration={0}>
-          //     <TooltipTrigger asChild>
-          //       <Link
-          //         href="#"
-          //         className={cn(
-          //           buttonVariants({ variant: link.variant, size: "icon" }),
-          //           "h-9 w-9",
-          //           link.variant === "default" &&
-          //             "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
-          //         )}
-          //       >
-          //         <link.icon className="h-4 w-4" />
-          //         <span className="sr-only">{link.title}</span>
-          //       </Link>
-          //     </TooltipTrigger>
-          //     <TooltipContent side="right" className="flex items-center gap-4">
-          //       {link.title}
-          //       {link.label && (
-          //         <span className="ml-auto text-muted-foreground">
-          //           {link.label}
-          //         </span>
-          //       )}
-          //     </TooltipContent>
-          //   </Tooltip>
-          // ) : (
             <Link
               key={index}
               href="#"
               className={cn(
                 buttonVariants({ variant: link.variant, size: "sm" }),
                 link.variant === "default" &&
-                  "dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
+                  "dark:bg-[--prodile-yellow] bg-[--prodile-yellow] dark:text-white dark:hover:bg-muted dark:hover:text-white",
                 "justify-start"
               )}
             >
